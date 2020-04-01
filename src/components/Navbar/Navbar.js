bd592b8a4ca4b60cee19ca7da632875b4da8ef4d
SimonevandenHam <sae.vandenham@gmail.com>
2020-04-01 15:59:36 +0200

    added sessionStorage to login action an reducer, user stays logged in after refreshing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,22 +2,49 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/navbar.css";
 
-export default class Navbar extends Component {
+class Navbar extends Component {
   render() {
-    return (
-      <div className="navbar">
-        <Link to="/" style={{ textDecoration: "none" }}>
-          <h3 className="home">ArtistList</h3>
-        </Link>
-        <div className="menuBox">
-          <Link to="/signup" style={{ textDecoration: "none" }}>
-            <h4 className="signUp">Sign up</h4>
+    if (this.props.login !== null) {
+      return (
+        <div className="navbar">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <h3 className="home">ArtistList</h3>
           </Link>
-          <Link to="/login" style={{ textDecoration: "none" }}>
-            <h4 className="login">Login</h4>
-          </Link>
+          <div className="menuBox">
+            <Link to="/myartistlist" style={{ textDecoration: "none" }}>
+              <h4 classname="myArtistList">My Artist List</h4>
+            </Link>
+            <Link to="/addconcert" style={{ textDecoration: "none" }}>
+              <h4 classname="addConcert">Add Concert</h4>
+            </Link>
+            <Link to="/logout" style={{ textDecoration: "none" }}>
+              <h4 classname="logout">Logout</h4>
+            </Link>
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="navbar">
+          <Link to="/" style={{ textDecoration: "none" }}>
+            <h3 className="home">ArtistList</h3>
+          </Link>
+          <div className="menuBox">
+            <Link to="/signup" style={{ textDecoration: "none" }}>
+              <h4 className="signUp">Sign up</h4>
+            </Link>
+            <Link to="/login" style={{ textDecoration: "none" }}>
+              <h4 className="login">Login</h4>
+            </Link>
+          </div>
+        </div>
+      );
+    }
   }
 }
+
+const mapStateToProps = state => ({
+  login: state.login
+});
+
+export default connect(mapStateToProps)(Navbar);
