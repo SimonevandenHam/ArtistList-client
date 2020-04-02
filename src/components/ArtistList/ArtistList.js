6cd63b614c6aa25bf6bd5e75227ac3388885b044
SimonevandenHam <sae.vandenham@gmail.com>
2020-04-02 10:28:27 +0200

    added the artistList reducer to the rood reducer

--- src/components/ArtistList/ArtistList.js
+++ src/components/ArtistList/ArtistList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
+import { getAllUserEvents } from "../../store/artistList/action";
+
 export class ArtistList extends Component {
   render() {
     return <div></div>;
@@ -9,6 +11,4 @@ export class ArtistList extends Component {
 
 const mapStateToProps = state => ({});
 
-const mapDispatchToProps = {};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ArtistList);
+export default connect(mapStateToProps, { getAllUserEvents })(ArtistList);
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -2,7 +2,13 @@ import { combineReducers } from "redux";
 import signup from "./signUp/reducer";
 import login from "./login/reducer";
 import artistSearchResults from "./addConcert/reducer";
+import artistList from "./artistList/reducer";
 
-const rootReducer = combineReducers({ signup, login, artistSearchResults });
+const rootReducer = combineReducers({
+  signup,
+  login,
+  artistSearchResults,
+  artistList
+});
 
 export default rootReducer;
