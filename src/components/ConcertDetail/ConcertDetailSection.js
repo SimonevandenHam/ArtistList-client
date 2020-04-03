dd01532496b3806f3a6afbc6ab1c5eb716617f33
SimonevandenHam <sae.vandenham@gmail.com>
2020-04-03 19:53:53 +0200

    made the folderstructure of the concert detail component and store

---
+++ src/components/ConcertDetail/ConcertDetailSection.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Paper from "@material-ui/core/Paper";
+
+export class ArtistListContainer extends Component {
+  componentDidMount() {}
+
+  render() {
+    return;
+  }
+}
+
+const mapStateToProps = state => ({});
+
+export default connect(mapStateToProps, {})(ArtistListContainer);
