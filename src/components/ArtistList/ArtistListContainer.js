21093acc14fc587ad466231df95faf8e20484a8f
SimonevandenHam <sae.vandenham@gmail.com>
2020-04-13 14:32:49 +0200

    added styling to the artist list

--- src/components/ArtistList/ArtistListContainer.js
+++ src/components/ArtistList/ArtistListContainer.js
@@ -14,35 +14,26 @@ export class ArtistListContainer extends Component {
     return this.props.artistList.map((concert) => {
       return (
         <Paper
+          className="paper"
           elevation={3}
           style={{ margin: 20, padding: 10, backgroundColor: "bisque" }}
         >
           <div key={concert.id}>
             {concert.artists.map((artist) => {
               return (
-                <div className="artistListBox">
-                  <div className="artistImageBox">
+                <div className="artistListBox" key={artist.id}>
+                  <div>
                     <img src={artist.artistPicture} alt="artist" />
                   </div>
                   <div className="concertInformationBox">
-                    <p
-                      style={{
-                        fontWeight: "bold",
-                        fontSize: 14,
-                        color: "#29434e",
-                      }}
-                      key={artist.id}
-                    >
-                      {artist.artist}
-                    </p>
-
-                    <p style={{ fontSize: 13, color: "#546e7a" }}>
-                      {concert.date}
-                    </p>
-                    <p style={{ fontSize: 13, color: "#546e7a" }}>
-                      {concert.venue}, {concert.location}
-                    </p>
-                  </div>{" "}
+                    <p className="artistName">{artist.artist}</p>
+                    <div className="concertInfo">
+                      <p>{concert.date}</p>
+                      <p>
+                        {concert.venue}, {concert.location}
+                      </p>
+                    </div>
+                  </div>
                 </div>
               );
             })}
