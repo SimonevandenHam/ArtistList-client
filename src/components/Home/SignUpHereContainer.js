d56f329b83faeca09defc0d69b8b4dc43a3cb31c
SimonevandenHam <sae.vandenham@gmail.com>
2020-04-16 10:10:36 +0200

    added two images to the homepage

--- src/components/Home/SignUpHereContainer.js
+++ src/components/Home/SignUpHereContainer.js
@@ -9,22 +9,53 @@ class signUpHereContainer extends React.Component {
   render() {
     return (
       <div className="homePage">
-        <div className="signUpHereContainer">
-          <h2 className="textContainer">
-            KEEP TRACK OFF ALL THE ARTISTS
-            <br />
-            YOU'VE SEEN LIVE
-          </h2>
-          <div className="buttonContainer">
-            <Button
-              color="primary"
-              variant="contained"
-              component={Link}
-              raised
-              to="/signup"
-            >
-              Sign up here
-            </Button>
+        <div>
+          <div className="signUpHereContainer">
+            <h2 className="textContainer">
+              KEEP TRACK OFF ALL THE ARTISTS
+              <br />
+              YOU'VE SEEN LIVE
+            </h2>
+            <div className="buttonContainer">
+              <Button
+                color="primary"
+                variant="contained"
+                component={Link}
+                raised
+                to="/signup"
+              >
+                Sign up here
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        <div className="imagesContainer">
+          <div className="imageContainer ticket">
+            <div class="button">
+              <Button
+                color="primary"
+                variant="contained"
+                component={Link}
+                raised
+                to="/addconcert"
+              >
+                add concert to my list
+              </Button>
+            </div>
+          </div>
+          <div className="imageContainer concert">
+            <div class="button">
+              <Button
+                color="primary"
+                variant="contained"
+                component={Link}
+                raised
+                to="/myartistlist"
+              >
+                go to my concert list
+              </Button>
+            </div>
           </div>
         </div>
       </div>
